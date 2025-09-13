095d798a57c4e2a1e13b759b25cb333ab6774d65
Uttam <uttamcse1@gmail.com>
2025-09-13 18:40:25 +0530

    Update Roms.java

--- src/main/java/com/example/demo/Roms.java
+++ src/main/java/com/example/demo/Roms.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class Roms {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FoodFrenzyApplication.class, args);
 	}
