326efa760f39fa2692c552ab0a27e0d06926accf
Uttam <uttamcse1@gmail.com>
2025-09-13 18:39:43 +0530

    Update and rename FoodFrenzyApplication.java to Roms.java

--- src/main/java/com/example/demo/Roms.java
+++ src/main/java/com/example/demo/Roms.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class FoodFrenzyApplication {
+public class Roms {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodFrenzyApplication.class, args);
